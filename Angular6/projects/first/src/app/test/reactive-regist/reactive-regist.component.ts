b011025a83366ce5b94238817f331ce0b934a2c5
forwardNow <wuqinfei_csdn@163.com>
2018-07-23 22:53:36 +0800

    【Angular6】<编辑> 表单校验: wan

--- Angular6/projects/first/src/app/test/reactive-regist/reactive-regist.component.ts
+++ Angular6/projects/first/src/app/test/reactive-regist/reactive-regist.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {composeAsyncValidators} from '@angular/forms/src/directives/shared';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-reactive-regist',
@@ -14,10 +14,17 @@ export class ReactiveRegistComponent implements OnInit {
 
   constructor(formBuilder: FormBuilder) {
     this.formModel = formBuilder.group({
-      username: [''],
+      // 内置校验器
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      // 自定义同步校验器
+      mobile: ['', this.mobileValidator],
+      // FromGroup 校验器器
       passwordGroup: formBuilder.group({
-        password: [''],
-        pwdRepeat: ['']
+        password: ['', Validators.required],
+        // 异步校验器，作为第三个参数
+        pwdRepeat: ['', '', this.asyncValidator]
+      }, {
+        validator: this.equalsValidator
       })
     });
   }
@@ -26,11 +33,38 @@ export class ReactiveRegistComponent implements OnInit {
   }
 
   handleSubmit() {
+    console.log('username', this.formModel.get('username').errors);
+    console.log('mobile', this.formModel.get('mobile').errors);
+    console.log('passwordGroup', this.formModel.get('passwordGroup').errors);
     console.log(this.formModel.value);
   }
 
-  xxxValidator(control: AbstractControl): { [key: string]: any } {
+  mobileValidator(control: AbstractControl): { [key: string]: any } {
+    const value = control.value;
+    if (!/[0-9]{3,}/.test(value)) {
+      return {
+        mobile: true
+      };
+    }
     return null;
   }
 
+  equalsValidator(group: FormGroup): { [key: string]: any } {
+    const password = group.get('password').value;
+    const pwdRepeat = group.get('pwdRepeat').value;
+    if (password !== pwdRepeat) {
+      return {
+        equals: {
+          msg: '不一致'
+        }
+      };
+    }
+    return null;
+  }
+
+  asyncValidator(control: AbstractControl): { [key: string]: any } {
+    const value = control.value;
+    const valid = /[0-9]{3,}/.test(value);
+    return Observable.of(valid ? null : {asyncV: true}).delay(2000);
+  }
 }
