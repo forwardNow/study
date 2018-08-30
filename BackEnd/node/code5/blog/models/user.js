848203084d78747a9cfca1cdbe1b3d8d6b144404
forwardNow <wuqinfei_csdn@163.com>
2018-08-30 22:10:59 +0800

    【node】<编辑> blog

--- BackEnd/node/code5/blog/models/user.js
+++ BackEnd/node/code5/blog/models/user.js
@@ -2,8 +2,51 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true });
 
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_time: {
+    type: Date,
+    // 默认值传入一个函数，当需要默认值时才会执行并获取值
+    default: Date.now,
+  },
+  last_modified_time: {
+    type: Date,
+    default: Date.now,
+  },
+  avatar: {
+    type: String,
+    default: '/public/img/avatar-default.png',
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+  gender: {
+    type: Number,
+    enum: [-1, 0, 1],
+    default: -1,
+  },
+  birthday: {
+    type: Date,
+  },
+  status: {
+    type: Number,
+    enum: [0, 1, 2],
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
--- BackEnd/node/code5/blog/routes/session.js
+++ BackEnd/node/code5/blog/routes/session.js
@@ -1,3 +1,5 @@
+const UserModel = require('../models/user');
+
 module.exports = (router) => {
   // 登陆页
   router.get('/login', (req, res) => {
@@ -16,10 +18,41 @@ module.exports = (router) => {
 
   // 处理注册
   router.post('/register', (req, res) => {
-    const { body } = req;
-    console.log(body);
-    res.send({
-      err_code: 0,
+    const { body: user } = req;
+
+    UserModel.findOne({
+      $or: [
+        { email: user.email },
+        { nickname: user.nickname },
+      ],
+    }).then((data) => {
+      // 不存在，允许注册
+      if (!data) {
+        return new UserModel(user).save();
+      }
+
+      // 邮箱已存在
+      if (data.email === user.email) {
+        return res.status(200).json({
+          err_code: 1,
+        });
+      }
+      // 昵称已存在
+      if (data.nickname === user.nickname) {
+        return res.status(200).json({
+          err_code: 2,
+        });
+      }
+      return null;
+    }).then(() => {
+      res.status(200).json({
+        err_code: 0,
+      });
+    }).catch((err) => {
+      console.log(err);
+      res.status(200).json({
+        err_code: 500,
+      });
     });
   });
 };
