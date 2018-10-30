00a1300a0defdf2f99df82925b957fec67ff570b
forwardNow <wuqinfei_csdn@163.com>
2018-10-30 21:52:03 +0800

    【Vue2.x】<编辑>webpack

--- FrontEnd/框架/Vue2.x/webpack/05-vue/src/App.vue
+++ FrontEnd/框架/Vue2.x/webpack/05-vue/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
     App Vue
-    <router-link to="/login">登陆</router-link>
-    <router-link to="/register">注册</router-link>
-    <router-link to="/management">管理</router-link>
-    <router-view></router-view>
-    <mt-button type="default">default</mt-button>
   </div>
 </template>
--- FrontEnd/框架/Vue2.x/webpack/05-vue/src/main.js
+++ FrontEnd/框架/Vue2.x/webpack/05-vue/src/main.js
@@ -1,17 +1,9 @@
 // import Vue from 'vue/dist/vue';
 import Vue from 'vue';
 
-// import MintUI from 'mint-ui';
-// import 'mint-ui/lib/style.css';
-// Vue.use(MintUI);
-
-import { Button } from 'mint-ui';
-
 import router from './router';
 
 import AppComponent from './App.vue';
-
-Vue.component(Button.name, Button);
 
 const app = new Vue({
   el: '#app',
