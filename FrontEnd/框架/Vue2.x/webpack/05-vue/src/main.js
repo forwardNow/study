a24c2a4c4ecfd87d72a80377a0ecce3309d64ff9
forwardNow <wuqinfei_csdn@163.com>
2018-10-30 22:10:33 +0800

    【Vue2.x】<编辑> vue项目：完成 header 和 footer

--- FrontEnd/框架/Vue2.x/webpack/05-vue/src/main.js
+++ FrontEnd/框架/Vue2.x/webpack/05-vue/src/main.js
@@ -1,16 +1,18 @@
-// import Vue from 'vue/dist/vue';
 import Vue from 'vue';
+import { Header, Tabbar, TabItem } from 'mint-ui';
 
 import router from './router';
 
 import AppComponent from './App.vue';
 
-const app = new Vue({
-  el: '#app',
+
+Vue.component(Header.name, Header);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+
+new Vue({
   render(createElements) {
     return createElements(AppComponent);
   },
   router,
-});
-
-console.log(app);
+}).$mount('#app');
