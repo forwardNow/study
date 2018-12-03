7e0a2890b0a47db9b83c49c1a3a4376311223ef1
forwardNow <wuqinfei_csdn@163.com>
2018-12-03 12:23:11 +0800

    【ES6】<编辑> let和const命令.md：4. 顶层对象的属性

---
+++ FrontEnd/JavaScript/ES6/demo/01-let和const/18-global.js
@@ -0,0 +1,5 @@
+window.num = 1;
+console.log(num); // 1
+
+num2 = 2;
+console.log(window.num2); // 2
---
+++ FrontEnd/JavaScript/ES6/demo/01-let和const/19-global2.js
@@ -0,0 +1,5 @@
+var num1 = 1;
+console.log(num1); // 1
+
+let num2 = 2;
+console.log(global.num2); // undefined
