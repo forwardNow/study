632485f2a379f02fad940d29842012727076af90
forwardNow <wuqinfei_csdn@163.com>
2019-01-16 21:36:23 +0800

    【React】<编辑> sample

--- FrontEnd/框架/React/sample/01.webpack-base/src/index.js
+++ FrontEnd/框架/React/sample/01.webpack-base/src/index.js
@@ -3,15 +3,20 @@ import ReactDom from 'react-dom';
 
 /**
  * 通过构造函数创建组件
- * @param proops {object} 接收外界传递过来的数据，props 是只读的
+ * @param props {object} 接收外界传递过来的数据，props 是只读的
  * @returns {JSX | string | null} 返回模板
  */
-function Hello(proops) {
-  return <p>Hello, { proops.name } !</p>;
+function Hello(props) {
+  return <p>Hello, { props.name }{ props.gender ? '先生' : '女士'} !</p>;
 }
 
+const data = {
+  name: '张三',
+  gender: 1,
+};
+
 const div = <div>
-  <Hello name='张三'></Hello>
+  <Hello { ...data }></Hello>
 </div>;
 
 ReactDom.render(div, document.querySelector('#app'));
